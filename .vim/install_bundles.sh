f5ef7b1b82d81e2f7e9d663a214c15adc67a93c1
etcet <git@etcet.net>
2013-02-07 14:33:05 -0600

    add install script for vim bundles

---
+++ .vim/install_bundles.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+sudo pacman -S cmake ruby
+# for YouCompleteMe
+tmp_dir=$(mktemp -d)
+cd "$tmp_dir"
+cmake -G 'Unix Makefiles' . ~/.vim/bundle/YouCompleteMe/cpp
+make ycm_core
+cd "$OLDPWD"
+rm -rf "$tmp_dir"
+
+# for Command-T
+cd ~/.vim/bundle/Command-T/ruby/command-t
+ruby extconf.rb
+make
+
+vim +BundleInstall +qall
